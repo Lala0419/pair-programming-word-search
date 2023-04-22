84b62e963ddf910cc02c2d6d2a86937f90125633
Nanaya Miki <nanayamiki@gmail.com>
2023-04-22 21:13:30 +0000

    pair programming with Irina.

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,40 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+// was developed by @lala0419  and @IrinaGM
 
-module.exports = wordSearch
+const transpose = (matrix) => {
+	let resultArr = [];
+	let placeholderArray = [];
+
+	// loop over the matrix where the outer loop is the column and the inner loop is the row
+	for (let c = 0; c < matrix[0].length; c++) {
+		for (let r = 0; r < matrix.length; r++) {
+			placeholderArray.push(matrix[r][c]);
+		}
+		resultArr.push(placeholderArray);
+		placeholderArray = []; // when we finish running over the row, reset the placeholderArray
+	}
+	return resultArr;
+};
+
+const wordSearch = (letters, word) => {
+	if (letters.length === 0 || word === "") {
+		return undefined;
+	}
+
+	const horizontalJoin = letters.map((ls) => ls.join(""));
+	for (let joinedWordH of horizontalJoin) {
+		if (joinedWordH.includes(word)) {
+			return true;
+		}
+	}
+
+	const verticalJoin = transpose(letters).map((ls) => ls.join(""));
+	for (let joinedWordV of verticalJoin) {
+		if (joinedWordV.includes(word)) {
+			return true;
+		}
+	}
+
+	return false;
+};
+
+module.exports = wordSearch;
